156e949c01f8344a4d1876c3e68018f50cf8010d
jorge sayago <sayagojorge2003@gmail.com>
2026-05-11 10:38:49 -0500

    refactor(Mensaje): Ahora el mensaje se muestra en un label dentro de la ventana

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,30 +23,44 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Button1 = New Button()
+        Label1 = New Label()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(202, 170)
+        Button1.ForeColor = Color.Black
+        Button1.Location = New Point(12, 6)
         Button1.Name = "Button1"
         Button1.Size = New Size(94, 27)
         Button1.TabIndex = 0
         Button1.Text = "Mensaje"
         Button1.UseVisualStyleBackColor = True
         ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = Color.SaddleBrown
+        Label1.Location = New Point(359, 13)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(0, 20)
+        Label1.TabIndex = 1
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.ButtonFace
         ClientSize = New Size(629, 364)
+        Controls.Add(Label1)
         Controls.Add(Button1)
         ForeColor = Color.Coral
         Name = "Form1"
         Text = "Solucion 1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -5,7 +5,12 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        MsgBox("Hola mundo")
+        Label1.Text = "Hola mundo"
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
 
     End Sub
 End Class
