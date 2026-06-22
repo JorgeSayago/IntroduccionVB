7f9fe0e5e1e6a48bbea72e8de76ebe796e2eb036
jorge sayago <sayagojorge2003@gmail.com>
2026-06-22 16:05:01 -0500

    feat(SelectColor): Implementamos un comboBox con la estructura select para cambiar el color de un panel

--- Pagina2.Designer.vb
+++ Pagina2.Designer.vb
@@ -22,9 +22,55 @@ Partial Class Pagina2
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Pagina2"
+        LblColor = New Label()
+        ComboBox1 = New ComboBox()
+        PnlHeader = New Panel()
+        PnlHeader.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' LblColor
+        ' 
+        LblColor.AutoSize = True
+        LblColor.Font = New Font("Segoe UI Emoji", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LblColor.Location = New Point(313, 34)
+        LblColor.Name = "LblColor"
+        LblColor.Size = New Size(163, 20)
+        LblColor.TabIndex = 0
+        LblColor.Text = "Seleccione un Color:"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Verde", "Azul", "Rojo", "Amarillo", "Sin color"})
+        ComboBox1.Location = New Point(492, 31)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 1
+        ' 
+        ' PnlHeader
+        ' 
+        PnlHeader.Controls.Add(LblColor)
+        PnlHeader.Controls.Add(ComboBox1)
+        PnlHeader.Dock = DockStyle.Top
+        PnlHeader.Location = New Point(0, 0)
+        PnlHeader.Name = "PnlHeader"
+        PnlHeader.Size = New Size(1010, 98)
+        PnlHeader.TabIndex = 2
+        ' 
+        ' Pagina2
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1010, 672)
+        Controls.Add(PnlHeader)
+        Name = "Pagina2"
+        Text = "Ejercicios 2"
+        PnlHeader.ResumeLayout(False)
+        PnlHeader.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents LblColor As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents PnlHeader As Panel
 End Class
--- Pagina2.vb
+++ Pagina2.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Pagina2
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Select Case ComboBox1.SelectedItem
+            Case "Verde"
+                PnlHeader.BackColor = Color.Green
+            Case "Azul"
+                PnlHeader.BackColor = Color.Blue
+            Case "Amarillo"
+                PnlHeader.BackColor = Color.Yellow
+            Case "Rojo"
+                PnlHeader.BackColor = Color.Red
+            Case Else
+                PnlHeader.BackColor = Color.Transparent
 
+        End Select
+
+    End Sub
 End Class
